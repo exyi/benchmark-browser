b3c16cc0c655a7c71c354afc42c79393524fede4
exyi <exyi@outlook.cz>
2018-09-30 12:45:42 +0000

    Implemented a workaround serializer for TestResultValue

--- src/data-access/DatabaseOperation.fs
+++ src/data-access/DatabaseOperation.fs
@@ -39,9 +39,78 @@ type FableHacksJsonConverter() = class
         let time = JValue.ReadFrom(reader).Value<float>()
         time * float TimeSpan.TicksPerMillisecond |> int64 |> TimeSpan |> box
 end
+let advance(reader: JsonReader) =
+    reader.Read() |> ignore
+let private readElements(reader: JsonReader, itemTypes: Type[], serializer: JsonSerializer) =
+    let rec read index acc =
+        match reader.TokenType with
+        | JsonToken.EndArray -> acc
+        | _ ->
+            let value = serializer.Deserialize(reader, itemTypes.[index])
+            advance reader
+            read (index + 1) (acc @ [value])
+    if reader.TokenType <> JsonToken.StartArray then failwithf"expected start of array: %A" reader.TokenType
+    advance reader
+    let x = read 0 List.empty
+    if reader.TokenType <> JsonToken.EndArray then failwithf"expected end of array: %A" reader.TokenType
+    x
+
+type TestResultValueJsonConverter() = class
+    inherit JsonConverter()
+    override x.CanRead = true
+    override x.CanWrite = true
+    override x.CanConvert(t: Type) =
+        // printfn "type: %s" t.FullName
+        t.FullName.StartsWith(typeof<TestResultValue>.FullName)
+
+    override x.WriteJson(writer: JsonWriter, value: obj, serializer: JsonSerializer) =
+        let value = value :?> TestResultValue
+        let name, object =
+            match value with
+            | TestResultValue.Time t -> "Time", box t
+            | TestResultValue.Number (n, s) -> "Number", box [|box n; box (Option.toObj s)|]
+            | TestResultValue.Fraction (n, s) -> "Fraction", box [|box n; box (Option.toObj s)|]
+            | TestResultValue.ByteSize s -> "ByteSize", box s
+            | TestResultValue.AttachedFile (id, s) -> "AttachedFile", box [|box id; box s|]
+            | TestResultValue.Anything str -> "Anything", box str
+        writer.WriteStartObject()
+        writer.WritePropertyName name
+        serializer.Serialize(writer, object)
+        writer.WriteEndObject()
+
+    override x.ReadJson(reader: JsonReader, t: Type, existingValue: obj, serializer: JsonSerializer) =
+        if reader.TokenType <> JsonToken.StartObject then failwith""
+        reader.Read() |> ignore
+        if reader.TokenType <> JsonToken.PropertyName then failwith""
+        let name = reader.Value :?> string
+        advance reader
+        let r =
+            match name with
+            | "Time" -> TestResultValue.Time (serializer.Deserialize<TimeSpan>(reader))
+            | "Number" ->
+                let v = readElements(reader, [|typeof<float>; typeof<string>|], serializer)
+                TestResultValue.Number(v.[0] :?> float, v.[1] :?> string |> Option.ofObj)
+            | "Fraction" ->
+                let v = readElements(reader, [|typeof<float>; typeof<string>|], serializer)
+                TestResultValue.Fraction(v.[0] :?> float, v.[1] :?> string |> Option.ofObj)
+            | "AttachedFile" ->
+                let v = readElements(reader, [|typeof<Guid>; typeof<string []>|], serializer)
+                TestResultValue.AttachedFile(v.[0] :?> Guid, v.[1] :?> string [])
+            | "Anything" -> TestResultValue.Anything (serializer.Deserialize<string>(reader))
+            | "ByteSize" -> TestResultValue.ByteSize (serializer.Deserialize<float>(reader))
+            | wtf -> failwithf "Can't deserialize bazmek %s" wtf
+
+        advance reader
+        if reader.TokenType <> JsonToken.EndObject then failwithf "expected and of object %A" reader.TokenType
+
+        box r
+
+
+end
 
 let converters = [|
         FableHacksJsonConverter() :> JsonConverter
+        TestResultValueJsonConverter() :> JsonConverter
         Fable.JsonConverter() :> JsonConverter
     |]
 
